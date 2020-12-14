f051fe37ca7acfa5e1b4e67b942573ee514d5051
Phan Trung Son <phantrungson@Phans-MacBook-Pro.local>
2020-12-14 21:13:47 +0700

    update

---
+++ Module2/bai11_java_collection_framework/thuc_hanh/cay_tim_kiem_nhi_phan/Tree.java
@@ -0,0 +1,7 @@
+package C1020G1.java_collection_framework.thuc_hanh.cay_tim_kiem_nhi_phan;
+
+public interface Tree<E> {
+    public boolean insert(E e);
+    public void inorder();
+    public int getSize();
+}
---
+++ Module2/bai11_java_collection_framework/thuc_hanh/cay_tim_kiem_nhi_phan/TreeNode.java
@@ -0,0 +1,11 @@
+package C1020G1.java_collection_framework.thuc_hanh.cay_tim_kiem_nhi_phan;
+
+
+public class TreeNode<E> {
+    protected E element;
+    protected TreeNode<E> left;
+    protected TreeNode<E> right;
+    public TreeNode(E e){
+        element = e;
+    }
+}
