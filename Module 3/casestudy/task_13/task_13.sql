585a91daf0976dd46ad9043ff7d90e98266d3537
Phan Trung Son <phantrungson@Phans-MacBook-Pro.local>
2021-01-14 14:53:20 +0700

    update

--- Module 3/casestudy/task_13/task_13.sql
+++ Module 3/casestudy/task_13/task_13.sql
@@ -1,3 +1,14 @@
-select dich_vu_di_kem.*, count(hop_dong_chi_tiet.id_dich_vu_di_kem) as dich_vu_di_kem_nhieu_nhat from dich_vu_di_kem join hop_dong_chi_tiet 
-on dich_vu_di_kem.id_dich_vu_di_kem = hop_dong_chi_tiet.id_dich_vu_di_kem
-group by dich_vu_di_kem.id_dich_vu_di_kem;
+select dich_vu_di_kem.*, count(*) as so_luong_dich_vu_di_kem
+from hop_dong_chi_tiet
+	join dich_vu_di_kem using(id_dich_vu_di_kem)
+group by id_dich_vu_di_kem
+having so_luong_dich_vu_di_kem in (
+	select max(so_luong_dich_vu_di_kem)
+	from (
+		select dich_vu_di_kem.*, count(*) as so_luong_dich_vu_di_kem
+		from hop_dong_chi_tiet
+			join dich_vu_di_kem using(id_dich_vu_di_kem)
+		group by id_dich_vu_di_kem
+	) as temp 
+); 
+
