d39c087f72b010f803ef0f97af7449e61a45a17f
Phan Trung Son <phantrungson@Phans-MacBook-Pro.local>
2021-01-18 16:10:20 +0700

    update

--- Module 3/casestudy/task_15/task_15.sql
+++ Module 3/casestudy/task_15/task_15.sql
@@ -1,9 +1,9 @@
 select nhan_vien.id_nhan_vien, nhan_vien.ho_ten, trinh_do.trinh_do, bo_phan.ten_bo_phan, nhan_vien.sdt, nhan_vien.diachi, 
 count(hop_dong.id_nhan_vien) as so_hop_dong_lap_duoc
 from nhan_vien
-join trinh_do on nhan_vien.id_trinh_do = trinh_do.id_trinh_do
-join bo_phan on nhan_vien.id_bo_phan = bo_phan.id_bo_phan
-join hop_dong on nhan_vien.id_nhan_vien = hop_dong.id_nhan_vien
-where year(hop_dong.ngay_lam_hop_dong) = 2018 or year(hop_dong.ngay_lam_hop_dong) = 2019
+left join trinh_do on nhan_vien.id_trinh_do = trinh_do.id_trinh_do
+left join bo_phan on nhan_vien.id_bo_phan = bo_phan.id_bo_phan
+left join hop_dong on nhan_vien.id_nhan_vien = hop_dong.id_nhan_vien
+where year(hop_dong.ngay_lam_hop_dong) in (2018, 2019)
 group by nhan_vien.id_nhan_vien
 having so_hop_dong_lap_duoc <= 3
--- Module 3/casestudy/task_20/task_20.sql
+++ Module 3/casestudy/task_20/task_20.sql
@@ -1,3 +1,3 @@
-select id_nhan_vien, ho_ten, email, ngay_sinh, sdt, diachi from nhan_vien
-union 
-select id_khach_hang, ho_ten, email, ngay_sinh, sdt, dia_chi from khach_hang;
+select id_nhan_vien, ho_ten, email, ngay_sinh, sdt, diachi, 'nhan vien' as chuc_vu from nhan_vien
+union all 
+select id_khach_hang, ho_ten, email, ngay_sinh, sdt, dia_chi, 'khach hang' as chuc_vu from khach_hang;
